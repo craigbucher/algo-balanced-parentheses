10c8d04ead5401daaad4ecb008a0a539ddf63102
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-27 16:35:35 -0500

    working python

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,34 @@
 balanceParens = (str) =>
 {
-
+  // array for indices of right parens unmatched with left parens	
+  let loneRight = [];	
+  // array for indices of right parens, matched or not	
+  let stack = [];	
+  for (let i = 0; i < str.length; i++) {	
+    // if left paren, push index to stack	
+    if (str[i] === '(') {	
+      stack.push(i);	
+    } else if (str[i] === ')') {	
+      // if unmatched right paren, push index to array	
+      if (stack.length === 0) {	
+        loneRight.push(i);	
+        // otherwise, remove corresponding left paren	
+      } else {	
+        stack.pop();	
+      }	
+    }	
+  }	
+  str = str.split('');	
+  // concat to create array of unmatched left and right parens	
+  loneRight = loneRight.concat(stack);	
+  // iterate and replace unmatched left and right parens with ''	
+  for (let i = 0; i < loneRight.length; i++) {	
+    str[loneRight[i]] = '';	
+  }	
+  // join array into string and return	
+  return str.join('');
 }
+
+//console.log(balanceParens(")(())("))
 
 module.exports = { balanceParens }
--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,26 @@
 def balance_parens(str):
-    pass
+    # array for indices of right parens unmatched with left parens	
+    loneRight = []
+    # array for indices of right parens, matched or not	
+    stack = []
+    for i in range(0, len(str)):
+        # if left paren, push index to stack	
+        if str[i] == '(':
+            stack.append(i)
+        elif str[i] == ')':
+            # if unmatched right paren, push index to array
+            if len(stack) == 0:
+                loneRight.append(i)
+            #otherwise, remove corresponding left paren
+            else:
+                stack.pop()
+    # split into array of characters
+    str = list(str)
+    # concat to create array of unmatched left and right parens
+    loneRight = loneRight + stack
+    # iterate and replace unmatched left and right parens with ''
+    for j in range(0, len(loneRight)):
+        str[loneRight[j]] = ''
+    # join array into string and return
+    return ''.join(str)
+#print(balance_parens(")(())("))    
